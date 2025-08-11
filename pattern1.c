0a3ac2cafc408c1997dae5335e6fe6b44230c3be
riiii-bit43 <megharaii15@gmail.com>
2025-08-11 18:04:41 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i==j){
+                printf("*");
+            }
+            printf("\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
